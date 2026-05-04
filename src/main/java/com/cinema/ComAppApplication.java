9ecc21115ee9ea0694cf17b07e7e764f28dd32a1
Mahjoub07 <mahjoubbounhar@gmail.com>
2026-05-04 03:01:06 +0100

    initial spring boot setup

---
+++ src/main/java/com/cinema/ComAppApplication.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComAppApplication.class, args);
+	}
+
+}
