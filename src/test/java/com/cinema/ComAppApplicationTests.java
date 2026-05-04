a67c27460ea7efbb25cb237aaa41f3dabaffd925
Mahjoub07 <mahjoubbounhar@gmail.com>
2026-05-04 17:47:52 +0100

    fix ComAppApplicationTests

--- src/test/java/com/cinema/ComAppApplicationTests.java
+++ src/test/java/com/cinema/ComAppApplicationTests.java
@@ -1,14 +1,11 @@
 package com.cinema;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class ComAppApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 }
